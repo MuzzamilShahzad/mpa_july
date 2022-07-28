859b1ef1fb31c8bd9ba79aa5c9964ea83130e435
muzzamil.laptop <muzzamil.shahzad20@gmail.com>
2022-07-28 16:36:48 +0500

    Updated migrations

--- database/migrations/2022_07_27_115230_create_student_feeses_table.php
+++ database/migrations/2022_07_27_115230_create_student_feeses_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('student_feeses', function (Blueprint $table) {
+            
             $table->increments('id');
 
-            $table->unsignedInteger('student_id');
-            $table->foreign('student_id')->references('id')->on('admissions')->onDelete('cascade');
+            $table->unsignedInteger('student_id')->nullable();
+            // $table->foreign('student_id')->references('id')->on('admissions')->onDelete('cascade');
             
             $table->unsignedInteger('fees_id');
             $table->foreign('fees_id')->references('id')->on('feeses')->onDelete('cascade');
@@ -25,20 +26,11 @@ return new class extends Migration
             $table->unsignedInteger('month_id')->nullable();
             $table->foreign('month_id')->references('id')->on('months')->onDelete('cascade');
 
-            $table->unique(['student_id','fees_id','month_id']);
-
-            // $table->unsignedInteger('fees_type_id');
-            // $table->foreign('fees_type_id')->references('id')->on('fees_types')->onDelete('cascade');
-
-            // $table->unsignedInteger('session_id');
-            // $table->foreign('session_id')->references('id')->on('sessions')->onDelete('cascade');
-            
-            // $table->unique(['student_id','fees_type_id','session_id','month_id']);
+            // $table->unique(['student_id','fees_id','month_id']);
 
             $table->integer('fees_amount');
             $table->tinyInteger('fee_discount')->nullable();
             $table->tinyInteger('fine')->nullable();
-            
             $table->string('note',60);
            
             $table->timestamps();
